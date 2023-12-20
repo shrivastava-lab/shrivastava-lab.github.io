f0bb0e86357369ce4014178952f7891f1c4b54ec
Indraneel Vairagare <indraneel207@gmail.com>
2023-12-20 22:53:14 +0530

    Separated Long data into separate data files

---
+++ app/people/_data.js
@@ -0,0 +1,106 @@
+const content = {
+  profile_picture: require('../../public/images/Abhishek Shrivastava.png'),
+  about: [
+    'I develop experimental & computational approaches to solve questions related to the microbiome, biofilms, collective motion, chemotaxis, molecular motors, and protein secretion. During my Postdoctoral training, I became interested in figuring out the factors that shape spatial structure of the human microbiota. We found that motile microbes of the phylum Bacteroidetes that are abundant in the human oral microbiome carry other non-motile bacteria as cargo and shape developing biofilms. Alongside, we also found a molecular rack and pinion machinery that couples with a bacterial Type 9 Secretion System (T9SS) and enables surface navigation.',
+    'My lab at Arizona State University combines biology, physics, and computer science to answer curiosity-driven questions that are mostly related to the Microbiome and Microbial systems biology. Many recent reports show that changes in the human microbiome correlate with the occurrence of periodontal disease, oral and colorectal cancers, obesity, diabetes and inflammatory bowel disease (IBD). We aim to find why and how the microbiome correlates with the above diseases and how we can use this information to design therapeutic strategies. On the Microbial Systems Biology front, we are interested in finding the nuts and bolts of a molecular rack and pinion machinery, driven by the T9SS. Please check the research section of this website for detailed information.',
+    'In spring, I teach a course titled Programming for Biologists. This course imparts a functional knowledge of Python. In fall, I teach a course titled Biology of microorganisms. Outside the lab, I enjoy tennis, cricket, volleyball, and random walks.'
+  ],
+  students: {
+    'Postdoctoral Fellows': [
+      {
+        name: 'Jitendra Gosai, PhD',
+        about:
+          'I am passionate about understanding how bacteria regulate complex physiological traits, especially ones that determine association with hosts. My current research includes understanding the regulation and biophysics of T9SS-mediated gliding motility. I use tools from ranging from bacterial genetics, protein biochemistry, microscopy and computational biology. Outside the lab, I like capturing stories on CMOS sensors.',
+        photo: require('../../public/images/Jitendra Gosai.png')
+      },
+      {
+        name: 'Abhishek Trivedi, PhD',
+        about:
+          'I am interested in learning more about the fundamentals of bacterial locomotion and deciphering the mechanism by which the type 9 secretion system controls gliding. I have a PhD in microbiology, and I am currently broadening my expertise by learning biophysics and computational biology. I am interested in doing fundamental microbiology research in future and I plan to stay in academics. Beside academics, I am interested reading literature, playing table tennis, running and cricket. ',
+        photo: require('../../public/images/Abhishek Trivedi.png')
+      },
+      {
+        name: 'Amanda Zdimal, PhD',
+        about:
+          'I am curious about the interactions between bacterial cells, their products, and the underlying mechanisms that influence human health. I aim to characterize spatial and temporal development of polymicrobial communities present in human mouth and gut. Since the oral and gut microbiomes have been implicated in numerous diseases, information regarding the microbial profile and development of community structure may provide new insights into the mechanisms which dictate healthy and disease states in humans.',
+        photo: require('../../public/images/Amanda Zdimal.png')
+      }
+    ],
+    'PhD students': [
+      {
+        name: 'Emma Henderson',
+        about:
+          'The wide array of microbial communities and the level of complexity to which such small organisms can achieve has always been a fascinating topic to me. During my undergraduate research career at Illinois State University, I focused on the metabolism taking place within Staphylococcal species. I have now transitioned into studying bacterial secretion system machinery, specifically the type  9 secretion system, that certain species utilize in order to carry out functions, like gliding motility. In my free time I enjoy doing puzzles, golfing, and enjoying the Arizona sun!',
+        photo: require('../../public/images/Emma Henderson.png')
+      },
+      {
+        name: 'Samyabrata Sen',
+        about:
+          'I am deeply intrigued by the mysteries of the microbiome. My expertise spans microbiology, biophysics, biochemistry, molecular biology, and coding. My current focus is on decoding the intricate motion of Flavobacterium johnsoniae. This investigation not only unveils microbial behavior but also offers insights for broader research endeavors. However, in the not-so-distant future, I plan to delve into the T9SS of Bacteroidetes, aiming to comprehend its role within the microbiome and its potential implications for health.',
+        photo: require('../../public/images/Samyabrata Sen.png')
+      },
+      {
+        name: 'Sofia Rocha',
+        about:
+          'I am a 2nd year PhD student in Microbiology. For my undergraduate, I studied at ASU with a major in Microbiology with a minor in Spanish. I joined the Shrivastava lab as an undergraduate researcher focusing on the role of motility within the human oral microbiome. My current research focuses on the Type 9 Secretion System and the human microbiome and how they influence each other. I am fascinated by the unseen world and love to look at the world at the microscopic level. My hobbies include hiking and traveling with my dogs and family.',
+        photo: require('../../public/images/Sofia Rocha.png')
+      },
+      {
+        name: 'Pavani Dadi',
+        about:
+          "I am a second-year Ph.D. student in Dr. Shrivastava's and Dr. Shah's labs, researching gut microbial enzymes and their metabolites influence on human physiology. Currently, we're focused on in-vitro characterization of enzymes from gut Bacteroides. In the future, I plan to explore the genetic manipulation of Bacteroides to study their potential links to neurodegenerative diseases. Outside of research, I enjoy cooking, painting, and drawing.",
+        photo: require('../../public/images/Pavani Dadi.png')
+      },
+      {
+        name: 'Aishwarya Patel',
+        about:
+          'I am a first-year PhD student working with Dr. Shrivastava and Dr. Shah. I am deeply interested in studying the human gut microbiota and observing their role in the gut-brain axis of the host. Outside the lab, I enjoy reading, hiking, and painting.',
+        photo: require('../../public/images/Aishwarya Patel.png')
+      }
+    ],
+    'Undergraduate Researchers': [
+      {
+        name: 'Arad Arasteh',
+        about:
+          'I am currently a Senior majoring in Biological Sciences (Genetics, Cell, and Developmental Biology). I plan to further my education and pursue a career in medicine. My interests are learning more about and further understanding biofilm formation and human microbiomes. I enjoy watching TV shows and playing soccer during my downtime.',
+        photo: require('../../public/images/Arad Arasteh.png')
+      },
+      {
+        name: 'Taryn Collins',
+        about:
+          'I am studying Molecular Biosciences and Biotechnology. I want to understand life at a molecular level and explain natural phenomena with their molecular functions. I am interested in the genetic role in bacterial motility, especially as seen in biofilms. Outside of school, I enjoy reading, swimming (not at the same time), and playing with my cats.',
+        photo: require('../../public/images/Taryn Collins.png')
+      },
+      {
+        name: 'Riya Garg',
+        about:
+          'I am studying Biomedical Sciences with a minor in Spanish. I am interested in learning about how microbial cells influence the overall health of humans. In the future, I hope to become a doctor and explore different parts of the world. In my free time, I enjoy hiking, dancing, spending time with friends and family, and listening to music.',
+        photo: require('../../public/images/Riya Garg.png')
+      }
+    ],
+    'Lab alumni': [
+      {
+        name: 'Maxim Averbukh',
+        about: '',
+        photo: require('../../public/images/Maxim Averbukh.png')
+      },
+      {
+        name: 'Kaxandra Nessie',
+        about: '',
+        photo: require('../../public/images/Kaxandra Nessie.png')
+      },
+      {
+        name: 'Madeleine Zheng',
+        about: '',
+        photo: require('../../public/images/Madeleine Zheng.png')
+      },
+      {
+        name: 'Claudia Brouwer',
+        about: '',
+        photo: require('../../public/images/Claudia Brouwer.png')
+      }
+    ]
+  }
+}
+
+export default content
