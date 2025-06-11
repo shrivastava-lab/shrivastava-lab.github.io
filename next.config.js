bafcec701f597f526e9100d251bc6558bdb031ae
Indraneel Vairagare <indraneel207@gmail.com>
2025-06-10 21:40:47 -0400

    fix: remove basePath configuration for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const isGithubPages = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true }, // if you're using next/image
-  basePath: '/shrivastava-lab.github.io', // Set the base path for GitHub Pages
 };
 
 module.exports = nextConfig;
